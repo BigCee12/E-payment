e58ec927042be6c9907f0e13a05b1bf32ae4644c
BigCee12 <reginaldejikeme852@gmail.com>
2023-10-13 21:41:45 +0100

    worked on the home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,8 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-         Colors.deepPurple, // Mixture of blue and orange
+      backgroundColor: Colors.cyanAccent, // Mixture of blue and orange
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -16,10 +15,13 @@ class HomeScreen extends StatelessWidget {
               'assets/images/loading_icon.png', // Replace with the path to your image
               height: 100, // Adjust the height as needed
             ),
-           const SizedBox(height: 5), // 5 pixels of space
-           const Text(
+            const SizedBox(height: 5), // 5 pixels of space
+            const Text(
               'E.Payment',
-              style: TextStyle(fontSize: 40, color: Color.fromARGB(255, 14, 13, 61), fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Color.fromARGB(255, 14, 13, 61),
+                  fontWeight: FontWeight.bold),
             ),
           ],
         ),
